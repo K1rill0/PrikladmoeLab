07c63294f1e15089e7e5a33a00ed9f3f51bb0541
Студент <@403-2>
2022-10-27 12:17:53 +0300

    Lab5

---
+++ Contracts/ICompanyRepository.cs
@@ -0,0 +1,11 @@
+﻿using ShopApi.Entities.Models;
+
+namespace ShopApi.Contracts;
+
+public interface ICompanyRepository : IRepositoryBase<Company>
+{
+    IEnumerable<Company> GetAllCompanies(bool trackChanges);
+    Company? GetCompany(Guid companyId, bool trackChanges);
+    void CreateCompany(Company company);
+    IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
+}
---
+++ Web.Api/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ShopApi.Entities.DataTransferObjects;
+using ShopApi.Entities.Models;
+
+namespace ShopApi.Web.Api;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Company, CompanyDto>()
+            .ForMember(c => c.FullAddress,
+                opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+        CreateMap<Employee, EmployeeDto>();
+        CreateMap<CompanyForCreationDto, Company>();
+        CreateMap<EmployeeForCreationDto, Employee>();
+    }
+}
